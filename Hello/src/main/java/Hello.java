d8d8667b0760510356f30e444d7b3d37a7881d64
just-cmm <1641303361@qq.com>
2021-09-01 16:30:32 +0800

    更新1

--- Hello/src/main/java/Hello.java
+++ Hello/src/main/java/Hello.java
@@ -6,4 +6,8 @@ public class Hello {
     public String sayHello(String name) {
         return "Hello "+name+"!";
     }
+
+    public static void main(String[] args) {
+        System.out.println("更新1");
+    }
 }
