9eee2259e803b60bb9e1c77dfdb19490c5b8a3ca
just-cmm <1641303361@qq.com>
2021-10-29 20:20:51 +0800

    分支添加

---
+++ DataStructures/src/com/cmm/sort/ShellSort.java
@@ -0,0 +1,46 @@
+package com.cmm.sort;
+
+/**
+ * @author cang
+ * @create 2021-10-29 17:19
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+
+    }
+
+    //交换式
+    public static void shellSort(int[] arr) {
+        int temp;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    //移位式
+    public static void shellSort2(int[] arr) {
+        int temp;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
